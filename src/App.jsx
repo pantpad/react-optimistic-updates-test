ce2062be17be2eee53e50b0dc2b6c83e2ac2b0f9
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-02 16:55:21 +0100

    Wrap app using queryClientProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import HomeMade from "./components/OptimisticHomeMade";
 
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 function App() {
   console.log("renderAPP");
   return (
     <>
-      <h1 className="mx-auto w-full">Optimistic</h1>
-      <HomeMade />
+      <QueryClientProvider client={queryClient}>
+        <h1 className="mx-auto w-full">Optimistic</h1>
+        <HomeMade />
+      </QueryClientProvider>
     </>
   );
 }
